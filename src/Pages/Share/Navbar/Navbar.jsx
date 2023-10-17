654b90bd34e77b8d3a1d77bcc07b442c2b824f28
niladhikari <niladhikari445@gmail.com>
2023-10-17 19:42:00 +0600

    add the navbar and footer and create some route and add this in the navbar

---
+++ src/Pages/Share/Navbar/Navbar.jsx
@@ -0,0 +1,97 @@
+import { Link, NavLink } from "react-router-dom";
+
+
+const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Products"> Add Product</NavLink>
+      </li>
+      <li>
+        <NavLink to="/carts">My Cart</NavLink>
+      </li>
+      
+    </>)
+
+const Navbar = () => {
+  const user = false;
+
+  
+  return (
+    <div>
+      <div className="md:mb-6 lg:mb-5">
+        <div className="navbar max-w-[1300px] mx-auto justify-between  px-5 lg:px-0">
+          <div className="navbar-start mt-3">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost md:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content font-bold mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {links}
+              </ul>
+            </div>
+            <a className="">
+              <img className="w-12 md:w-16" src="https://i.ibb.co/D78HW2S/download-1.png" alt="" />
+            </a>
+          </div>
+          <div className="navbar-center hidden md:flex">
+            <ul className="menu font-bold menu-horizontal px-1">{links}</ul>
+          </div>
+          <div className="navbar-end">
+            {user?.email ? (
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img className="bg-slate-500" src={user.photoURL} alt={user.displayName} />
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <button className="btn btn-sm  btn-ghost">
+                      {user.displayName}
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={'handleLogout'}
+                      className="btn btn-sm  btn-ghost font-bold text-red-500"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/login">
+                <button className="btn  p-2 bg-gray-300 text-center font-bold text-green-800">Login</button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+
     },
   ]);
 
