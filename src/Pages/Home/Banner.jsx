50a78a1cfa110973ed7c879cc663330edfe0e428
niladhikari <niladhikari445@gmail.com>
2023-10-21 21:50:17 +0600

    update the responsive ness for the tab

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,8 +1,8 @@
 import "./Banner.css";
 const Banner = () => {
   return (
-    <div className="mt-6 md:mt-1 max-w-[1300px] mx-auto background py-4 md:py-0 md:h-[80vh] grid justify-center items-center">
-      <div className=" flex flex-col-reverse md:flex-row justify-around gap-6 items-center px-6">
+    <div className="mt-6 lg:mt-1 max-w-[1300px] mx-auto background py-4 lg:py-0 lg:h-[80vh] grid justify-center items-center">
+      <div className=" flex flex-col-reverse lg:flex-row justify-around gap-6 items-center px-6">
         <h2 className="text-center text-xl md:text-3xl font-bold text-pink-500 mb-3 md:mb-0">
         Technology and electronics encompass a vast array of innovative devices, from smartphones and laptops to cutting-edge gadgets.
         </h2>
